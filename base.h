b69e655bbec70080e3e6c8e045fc409becb4931a
uniren <darkluna0628@gmail.com>
2024-12-11 21:18:09 +0900

    초기커밋

---
+++ base.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <stdio.h>
+#include <windows.h>
+
+void love() {
+	// Sleep(5000);
+	printf("\n \n================================================= \n");
+	printf("C_현_IDE : 프로그램 종료할께. \n");
+	printf("윤아, 잘했어. 프로그래머에 1bit 정도 가까워졌네.\n");
+}
---
+++ practice_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "base.h"
+void swap(int* a, int* b);
+
+int main()
+{
+	int a = 120;
+	int b = 240;
+	printf("before_%d %d", a, b);
+	swap(&a, &b);
+	printf("after_%d %d", a, b);
+	return 0;
+}
+
+void swap(int* a, int* b)
+{
+	int c = *b;
+	*b = *a;
+	*a = c;
+}
